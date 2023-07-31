86e8337bcb810e2cb5f4a056e5e0bf864e9e4ef5
Rishabh <romantimeless@gmail.com>
2023-08-01 00:09:02 +0530

    created stateful myhomepage class

--- skindoc/lib/main.dart
+++ skindoc/lib/main.dart
@@ -23,3 +23,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/images/friendly_health_logo.png',
+              width: 500,
+              height: 500), // Replace 'assets/logo.png' with your logo image
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SkinLesionPage()),
+            ),
+            child: const Text('Check Skin Lesion'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const PreventionTipsScreen()),
+            ),
+            child: const Text('Skin Cancer Tips'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => exit(0),
+            child: const Text('Exit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
