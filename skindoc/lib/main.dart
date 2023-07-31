3b5cdae4c560d5b2d376307ea7dc459b81d9d488
Rishabh <romantimeless@gmail.com>
2023-08-01 00:18:59 +0530

    created show picker function to open cam or gal

--- skindoc/lib/main.dart
+++ skindoc/lib/main.dart
@@ -108,4 +108,38 @@ class _SkinLesionScreenState extends State<SkinLesionScreen> {
       print('Error loading model: $e');
     }
   }
+
+  void _showPicker({
+    required BuildContext context,
+  }) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Photo Library'),
+                onTap: () {
+                  getImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  getImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  
 }
