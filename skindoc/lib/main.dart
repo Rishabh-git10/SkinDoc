46eedd2a16c3b2c1369b547a3d71681b2c703e2e
Rishabh <romantimeless@gmail.com>
2023-08-01 00:21:48 +0530

    created predict fun to get the out rec by getimage

--- skindoc/lib/main.dart
+++ skindoc/lib/main.dart
@@ -226,5 +226,19 @@ class _SkinLesionScreenState extends State<SkinLesionScreen> {
     return input.buffer.asUint8List();
   }
 
-  
+  Float32List predict(Uint8List input) {
+  if (_interpreter == null) {
+    throw Exception('Model not loaded yet.');
+  }
+
+  final output = List<List<double>>.filled(1, List<double>.filled(7, 0.0));
+  _interpreter!.run(input, output);
+
+  // Extract the first (and only) element of the 2D list, then convert it to a Float32List
+  final List<double> probabilities = output[0];
+  final Float32List result = Float32List.fromList(probabilities);
+
+  return result;
+  }
+
 }
