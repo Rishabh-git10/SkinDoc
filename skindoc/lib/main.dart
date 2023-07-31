e2892b7a13d332b00cdbef027cac94bce0a66d42
Rishabh <romantimeless@gmail.com>
2023-08-01 00:07:43 +0530

    created myapp class for initialization

--- skindoc/lib/main.dart
+++ skindoc/lib/main.dart
@@ -6,3 +6,20 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
 import 'package:image/image.dart' as img;
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Skin Cancer Detection',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Skin Cancer Detection'),
+    );
+  }
+}
+
